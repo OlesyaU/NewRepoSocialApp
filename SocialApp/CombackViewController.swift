f7816a27b853de72be887ec0a2c1064a6ca6591d
Name <your_email@example.com>
2023-07-17 00:49:45 +0300

    commit

--- SocialApp/CombackViewController.swift
+++ SocialApp/CombackViewController.swift
@@ -66,11 +66,7 @@ final class CombackViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
         configure()
     }
-
-
-
-
-    
+ 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
